8cdc3e77cddd537a316fa62d5bf8f67ff4f5bca0
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2023-06-03 23:40:32 -0500

    added warnings in traduction and uml for explanation

--- src/ddl2umltrad.java
+++ src/ddl2umltrad.java
@@ -4,7 +4,7 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 
 public class ddl2umltrad extends ddl2umltranslgrammarBaseListener{
     String translatedoutput = "@startuml \n !theme crt-amber \n";
@@ -43,7 +43,21 @@ public class ddl2umltrad extends ddl2umltranslgrammarBaseListener{
     @Override
     public void enterCreateTableStatement(ddl2umltranslgrammarParser.CreateTableStatementContext ctx) {
         String translated = "";
+        String warnings = "\n warnings: \n";
+        List<String> columnsList = new ArrayList<String>();
+        Set<String> columnsSet = new HashSet<String>();
+        for (int i = 0; i < ctx.columnDefinition().size(); i++) {
+            columnsList.add(ctx.columnDefinition(i).columnName().getText());
+            columnsSet.add(ctx.columnDefinition(i).columnName().getText());
+            if(ctx.columnDefinition(i).columnName().getText().substring(ctx.columnDefinition(i).columnName().getText().length()-1).equals("s")){
+                warnings = warnings + "Posible non atomic values stored on column "+ ctx.columnDefinition(i).columnName().getText() + " \n";
+            }
+        }
+        if (columnsList.size() != columnsSet.size()){
+            warnings = warnings + "Possible presence of repeated columns \n";
+        }
         translated = translated + "entity "+ctx.tableName().getText()+" { \n";
+        boolean primkey = false;
         for (int i = 0; i < ctx.columnDefinition().size(); i++) {
             translated = translated + "{field}";
             if (ctx.columnDefinition(i).constraint().isEmpty()){
@@ -57,6 +71,7 @@ public class ddl2umltrad extends ddl2umltranslgrammarBaseListener{
                     }
                     if (ctx.columnDefinition(i).constraint(j).PRIMARY() != null) {
                         translated = translated + "* ";
+                        primkey = true;
                     }
                     if (!nameadded) {
                         translated = translated + ctx.columnDefinition(i).columnName().getText() + " " + ctx.columnDefinition(i).datatype().getText();
@@ -79,13 +94,26 @@ public class ddl2umltrad extends ddl2umltranslgrammarBaseListener{
             }
             translated = translated + " \n";
         }
-        for (int i = 0; i < ctx.foreignKeyDefiniton().size(); i++) {
-            translated = translated + "{method} " +ctx.foreignKeyDefiniton(i).columnName(0).getText() +" "+
-                    columntypes.get(ctx.foreignKeyDefiniton(i).tableName().getText()+":"+ctx.foreignKeyDefiniton(i).columnName(1).getText())+ " <<FK>> \n";
+        if (!primkey){
+            warnings = warnings + "No primary key defined. \n";
         }
+        for (int i = 0; i < ctx.foreignKeyDefiniton().size(); i++) {
+            if(columntypes.get(ctx.foreignKeyDefiniton(i).tableName().getText()+":"+ctx.foreignKeyDefiniton(i).columnName(1).getText()) == null){
+                warnings = warnings + "Foreign key "+ctx.foreignKeyDefiniton(i).columnName(0).getText()+" not defined in other tables. \n";
+            }else{
+                translated = translated + "{method} " +ctx.foreignKeyDefiniton(i).columnName(0).getText() +" "+
+                        columntypes.get(ctx.foreignKeyDefiniton(i).tableName().getText()+":"+ctx.foreignKeyDefiniton(i).columnName(1).getText())+ " <<FK>> \n";
+            }
+            if (!columnsSet.contains(ctx.foreignKeyDefiniton(i).columnName(0).getText())){
+                warnings = warnings + "Foreign key "+ctx.foreignKeyDefiniton(i).columnName(0).getText()+" not in defined columns. \n";
+            }
+        }
+        translated = translated + warnings;
         translated= translated + "} \n \n";
         for (int i = 0; i < ctx.foreignKeyDefiniton().size(); i++) {
-            translated = translated + ctx.tableName().getText() + " }o..|| "+ ctx.foreignKeyDefiniton(i).tableName().getText() +" \n";
+            if(columntypes.get(ctx.foreignKeyDefiniton(i).tableName().getText()+":"+ctx.foreignKeyDefiniton(i).columnName(1).getText()) != null){
+                translated = translated + ctx.tableName().getText() + " }o..|| "+ ctx.foreignKeyDefiniton(i).tableName().getText() +" \n";
+            }
         }
         translated= translated + " \n";
         translatedoutput = translatedoutput + translated;
@@ -108,6 +136,4 @@ public class ddl2umltrad extends ddl2umltranslgrammarBaseListener{
         viewcount ++;
         super.enterCreateViewStatement(ctx);
     }
-
-
 }
